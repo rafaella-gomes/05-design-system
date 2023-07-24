86726a644da6dc36545219d12b5c531265ac66f9
Rafaella <rafaella@Air-de-Rafaella.lan>
2023-07-24 21:46:19 +0100

    fix: error on storybook page

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,29 +1,36 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: [
-    "../src/pages/**/*.stories.mdx", 
-    "../src/stories/**/*.stories.tsx"],
-  addons: [
+module.exports = {
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y"
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  features: {
-   "storyStoreV7":true
+  "features": {
+    "storyStoreV7": true
   },
-  docs: {
-    autodocs: "tag",
-  },
-  viteFinal: (config, {configType}) => {
-    if(configType === 'PRODUCTION') {
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
       config.base = '/05-design-system/'
     }
-    return config
+
+    return {
+      ...config,
+      define: {
+        ...config.define,
+        global: "window",
+      },
+      esbuild: {
+        ...config.esbuild,
+        jsxInject: `import React from 'react'`,
+      },
+    };
   }
-};
-export default config;
+}
